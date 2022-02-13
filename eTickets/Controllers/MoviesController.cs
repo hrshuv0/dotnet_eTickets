f77b90c61e2e2c112d2a5e1559520a5092b6f80b
shuvo <hrshuva9@gmail.com>
2022-02-14 02:15:34 +0600

    added search option

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -27,6 +27,20 @@ namespace eTickets.Controllers
             return View(allMovies);
         }
         
+        public async Task<IActionResult> Filter(string searchString)
+        {
+            var allMovies = await _service.GetAllAsync(m => m.Cinema);
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var filteredResult = allMovies.Where(n => n.Name.Contains(searchString) ||
+                                                          n.Description.Contains(searchString)).ToList();
+                return View("Index", filteredResult);
+            }
+            
+            return View("Index", allMovies);
+        }
+        
         //GET: Movies/Details/1
         public async Task<IActionResult> Details(int id)
         {
